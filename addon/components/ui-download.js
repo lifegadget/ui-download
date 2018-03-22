a6d74f7e5f8000276de4a22681c69dd5a7e7c0f8
Ken Snyder <ken@ken.net>
2018-03-22 03:19:47 -0700

    allow thenable data

--- addon/components/ui-download.js
+++ addon/components/ui-download.js
@@ -1,26 +1,24 @@
-import Ember from 'ember';
+import Ember from "ember";
 const { keys, create } = Object; // jshint ignore:line
-const { computed, observer, $, run, on, typeOf, debug, isPresent } = Ember;  // jshint ignore:line
+const { computed, observer, $, run, on, typeOf, debug, isPresent } = Ember; // jshint ignore:line
 const { defineProperty, get, set, inject, isEmpty, merge } = Ember; // jshint ignore:line
 const a = Ember.A; // jshint ignore:line
 
-import layout from '../templates/components/ui-download';
+import layout from "../templates/components/ui-download";
 
 export default Ember.Component.extend({
   layout: layout,
-  classNameBindings: ['_file','hasBeenDownloaded:downloaded:not-downloaded'],
-  classNames: ['ui-download'],
+  classNameBindings: ["_file", "hasBeenDownloaded:downloaded:not-downloaded"],
+  classNames: ["ui-download"],
+  /** data or a promise that resolves to data */
   data: null,
-  id: null, // this property isn't used but it is often handy for container to put an ID reference here
-  _data: computed('data', function() {
-    return this.get('data') ? 'data-ready' : null;
-  }),
-  size: 'normal',
-  mime: 'text/plain',
-  charset: 'utf-8',
-  url: computed('mime', 'data', function() {
-    let {mime, data, charset} = this.getProperties('mime', 'data', 'charset');
-    data = charset === 'utf-8' || mime.slice(0, 4) === 'text' ? encodeURIComponent(data) : data;
+  id: null,
+  size: "normal",
+  mime: "text/plain",
+  charset: "utf-8",
+  url: computed("mime", "data", function() {
+    let { mime, data, charset } = this.getProperties("mime", "data", "charset");
+    data = charset === "utf-8" || mime.slice(0, 4) === "text" ? encodeURIComponent(data) : data;
     return `data:${mime};charset=${charset},${data}`;
   }),
   supportsFileSaver: computed(function() {
@@ -28,36 +26,52 @@ export default Ember.Component.extend({
   }),
   hasBeenDownloaded: false,
   click(evt) {
-    if(evt.target.className !== 'hidden-link') {
-      this.set('hasBeenDownloaded', true);
-      const {data, filename, elementId, charset, mime} = this.getProperties('data','filename','elementId', 'charset', 'mime');
-      // Prefer FileSaver strategy (should work on all modern browsers)
-      if(this.get('supportsFileSaver')) {
-        let blob = new Blob([data], {type: `${mime};charset=${charset}`});
-        window.saveAs(blob, filename);
-      } else {
-        if(window.navigator.msSaveOrOpenBlob) {
-          // Microsoft Strategy
-          let fileData = [data];
-          let blobObject = new Blob(fileData);
-          window.navigator.msSaveOrOpenBlob(blobObject, filename);
-        } else {
-          // Non-microsoft startegy
-          $(`#${elementId} .hidden-link`)[0].click();
-        }
-      }
+    if (evt.target.className !== "hidden-link") {
+      this.set("hasBeenDownloaded", true);
+      const { data, filename, elementId, charset, mime } = this.getProperties(
+        "data",
+        "filename",
+        "elementId",
+        "charset",
+        "mime"
+      );
+      console.log("Making sure data is thenable. ", data.then ? true : false);
+      const thenableData = data.then
+        ? data
+        : {
+            then: () => data
+          };
+      thenableData.then(datum => {
+        console.log("file content ready");
 
-      if(typeOf(this.attrs.onDownload) === 'function') {
-        this.attrs.onDownload({
-          event: evt,
-          object: this
-        });
-      } else {
-        this.sendAction('onDownload', {
-          event: evt,
-          object: this
-        });
-      }
+        // Prefer FileSaver strategy (should work on all modern browsers)
+        if (this.get("supportsFileSaver")) {
+          let blob = new Blob([datum], { type: `${mime};charset=${charset}` });
+          window.saveAs(blob, filename);
+        } else {
+          if (window.navigator.msSaveOrOpenBlob) {
+            // Microsoft Strategy
+            let fileData = [datum];
+            let blobObject = new Blob(fileData);
+            window.navigator.msSaveOrOpenBlob(blobObject, filename);
+          } else {
+            // Non-microsoft startegy
+            $(`#${elementId} .hidden-link`)[0].click();
+          }
+        }
+
+        if (typeOf(this.attrs.onDownload) === "function") {
+          this.attrs.onDownload({
+            event: evt,
+            object: this
+          });
+        } else {
+          this.sendAction("onDownload", {
+            event: evt,
+            object: this
+          });
+        }
+      });
     }
   }
 });
--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 /* jshint node: true */
-'use strict';
+"use strict";
 
 module.exports = {
-  name: 'ui-download',
+  name: "ui-download",
   normalizeEntityName: function() {},
   included: function(app, parentAddon) {
-    var target = (parentAddon || app);
-    target.import('vendor/ui-download/ui-download.css');
-    target.options.babel = target.options.babel || { includePolyfill: true };
-    target.import('bower_components/filesaver/FileSaver.js');
+    var target = parentAddon || app;
+    target.import("vendor/ui-download/ui-download.css");
+    // target.options.babel = target.options.babel || { includePolyfill: true };
+    target.import("bower_components/filesaver/FileSaver.js");
   }
 };
